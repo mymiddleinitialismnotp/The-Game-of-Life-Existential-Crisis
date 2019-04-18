297e57449abf6f859fe57ef030dfbac492c00058
Kyla P Wright <kw38531@srcs.loc>
2019-04-18 14:24:17 -0700

    javascript mod

--- golec.js
+++ golec.js
@@ -42,6 +42,9 @@ class Word {
     }
 }
 
+    let goodwords = ["small animals", "batteries", "green lights", "sleep", "ice cream", "chickens", "small plants", "pay raise", "music", "art"];
+    let badwords = ["anxiety", "depression", "taxes", "dictators", "eyebrow mites", "homework", "moist", "tourists", "sneezing", "dirty dishes", "wet socks", "weak handshakes", "overexplaining", "student debt", "sweat"];
+
 
 
 
