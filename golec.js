d0dfe825ce793cc035c8bbf92728ac8b641e2a11
Aliya M Clark <ac92170@srcs.loc>
2019-04-22 14:18:55 -0700

    added GameOfLife object, created keypress event listener, started function to move player between columns

--- golec.js
+++ golec.js
@@ -1,10 +1,25 @@
+class GameOfLife {
+    constructor(){
+        this.player = new Player()
+        window.addEventListener('keydown', (e) => {
+            if(e.keyCode == 37) {
+                console.log("move left!");
+                this.player.changeColumnPosition(-1);
+                this.player.render();
+            }else if(e.keycode == 39) {
+                console.log(move)
+                this.player.changeColumnPosition(+1);
+                this.player.render();
+            })
+        })
+    }
+}
+
 class Player {
     constructor(_xpos, _ypos, _imgsrc) {
-        this.xpos = _xpos;
-        this.ypos = 0;
+        this.colPos = 2;
         this.imgsrc = _imgsrc;
-        this.RIGHT = x;
-        this.LEFT = x;
+        this.div = document.getElementById("player");
     }
 
     playerWordDeath() {
@@ -14,44 +29,64 @@ class Player {
     playerCatchWinnerWord() {
 
     }
-}
 
-
-
-
-class Anvil {
-    constructor(_xpos, _ypos, _imgsrc) {
-        this.xpos = _xpos;
-        this.ypos = _ypos;
-        this.imgsrc = _imgsrc;
+    changeColumnPosition(delta){
+        this.colPos = this.colPos + delta;
+        console.log(this.colPos);
     }
 
-     smashPlayer() {
+    render() {
+        let parent = this.div.parentNode;
 
-    }
-}
+        if(this.colPos == 2) {
+            // This is where you need to remove it from one parent column
+            // And then append it as a child to a new parent column
 
-
-
-
-class Word {
-    constructor(_xpos, _ypos, _imgsrc) {
-        this.xpos = _xpos;
-        this.ypos = _ypos;
-        this.imgsrc = _imgsrc;
+        }
     }
 }
 
-    let goodwords = ["small animals", "batteries", "green lights", "sleep", "ice cream", "chickens", "small plants", "pay raise", "music", "art"];
-    let badwords = ["anxiety", "depression", "taxes", "dictators", "eyebrow mites", "homework", "moist", "tourists", "sneezing", "dirty dishes", "wet socks", "weak handshakes", "overexplaining", "student debt", "sweat"];
+//
+//
+//
+//class Anvil {
+//    constructor(_xpos, _ypos, _imgsrc) {
+//        this.xpos = _xpos;
+//        this.ypos = _ypos;
+//        this.imgsrc = _imgsrc;
+//    }
+//
+//     smashPlayer() {
+//
+//    }
+//}
+//
+//
+//
+//
+//class Word {
+//    constructor(_xpos, _ypos, _imgsrc) {
+//        this.xpos = _xpos;
+//        this.ypos = _ypos;
+//        this.ystep = _ystep;
+//      this.color = _color;
+//      this.elem = document.getElementById(_id);
+//
+//    }
+//}
+//
+//    let goodwords = ["small animals", "batteries", "green lights", "sleep", "ice cream", "chickens", "small plants", "pay raise", "music", "art"];
+//    let badwords = ["anxiety", "depression", "taxes", "dictators", "eyebrow mites", "homework", "moist", "tourists", "sneezing", "dirty dishes", "wet socks", "weak handshakes", "overexplaining", "student debt", "sweat"];
+//
+//
+//
+//
+//class Healthbar {
+//    constructor(_xpos, _ypos, _imgsrc) {
+//        this.xpos = _xpos;
+//        this.ypos = _ypos;
+//        this.imgsrc = _imgsrc;
+//    }
+//}
 
-
-
-
-class Healthbar {
-    constructor(_xpos, _ypos, _imgsrc) {
-        this.xpos = _xpos;
-        this.ypos = _ypos;
-        this.imgsrc = _imgsrc;
-    }
-}
+let myApp = new GameOfLife();
