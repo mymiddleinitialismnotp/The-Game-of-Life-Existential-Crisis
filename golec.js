924495aaedd8cde24e9ad25487d2000ae09b02b8
Kyla P Wright <kw38531@srcs.loc>
2019-04-24 13:50:51 -0700

    js mod

--- golec.js
+++ golec.js
@@ -39,54 +39,55 @@ class Player {
         let parent = this.div.parentNode;
 
         if(this.colPos == 2) {
-            // This is where you need to remove it from one parent column
+             //This is where you need to remove it from one parent column
             // And then append it as a child to a new parent column
 //https://www.w3schools.com/jsref/met_node_removechild.asp
         }
     }
 }
 
-//
-//
-//
-//class Anvil {
-//    constructor(_xpos, _ypos, _imgsrc) {
-//        this.xpos = _xpos;
-//        this.ypos = _ypos;
-//        this.imgsrc = _imgsrc;
-//    }
-//
-//     smashPlayer() {
-//
-//    }
-//}
-//
-//
-//
-//
-//class Word {
-//    constructor(_xpos, _ypos, _imgsrc) {
-//        this.xpos = _xpos;
-//        this.ypos = _ypos;
-//        this.ystep = _ystep;
-//      this.color = _color;
-//      this.elem = document.getElementById(_id);
-//
-//    }
-//}
-//
-//    let goodwords = ["small animals", "batteries", "green lights", "sleep", "ice cream", "chickens", "small plants", "pay raise", "music", "art"];
-//    let badwords = ["anxiety", "depression", "taxes", "dictators", "eyebrow mites", "homework", "moist", "tourists", "sneezing", "dirty dishes", "wet socks", "weak handshakes", "overexplaining", "student debt", "sweat"];
-//
-//
-//
-//
-//class Healthbar {
-//    constructor(_xpos, _ypos, _imgsrc) {
-//        this.xpos = _xpos;
-//        this.ypos = _ypos;
-//        this.imgsrc = _imgsrc;
-//    }
-//}
 
+
+
+class Anvil {
+    constructor(_xpos, _ypos, _imgsrc) {
+        this.xpos = _xpos;
+       this.ypos = _ypos;
+       this.imgsrc = _imgsrc;
+    }
+
+     smashPlayer() {
+
+   }
+}
+
+
+
+
+class Word {
+    constructor(_xpos, _ypos, _imgsrc) {
+        this.xpos = _xpos;
+        this.ypos = _ypos;
+       this.ystep = _ystep;
+     this.color = _color;
+     this.elem = document.getElementById(_id);
+
+    }
+}
+
+   let goodwords = ["small animals", "batteries", "green lights", "sleep", "ice cream", "chickens", "small plants", "pay raise", "music", "art"];
+   let badwords = ["anxiety", "depression", "taxes", "dictators", "eyebrow mites", "homework", "moist", "tourists", "sneezing", "dirty dishes", "wet socks", "weak handshakes", "overexplaining", "student debt", "sweat"];
+
+
+
+
+class Healthbar {
+    constructor(_xpos, _ypos, _imgsrc) {
+        this.xpos = _xpos;
+        this.ypos = _ypos;
+        this.imgsrc = _imgsrc;
+   }
+}
+
+let healthValue = (Math.ceil * (Math.random()*100))
 let myApp = new GameOfLife();
