473d893568b04a197c11da5629a0c0962945ce06
Aliya M Clark <ac92170@srcs.loc>
2019-04-26 13:09:32 -0700

    Hi Mr. Temple, added code from animation assn and will edit to fit our needs after

--- golec.js
+++ golec.js
@@ -73,24 +73,61 @@ class Anvil {
 
 
 class Word {
-    constructor(_xpos, _ypos, _imgsrc) {
+    constructor(_xpos, _ypos, _ystep, _id) {
         this.xpos = _xpos;
         this.ypos = _ypos;
         this.ystep = _ystep;
-        this.color = _color;
         this.elem = document.getElementById(_id);
         this.goodWords = ["small animals", "batteries", "green lights", "sleep", "ice cream", "chickens", "small plants", "pay raise", "music", "art"];
         this.badWords = ["anxiety", "depression", "taxes", "dictators", "eyebrow mites", "homework", "moist", "tourists", "sneezing", "dirty dishes", "wet socks", "weak handshakes", "overexplaining", "student debt", "sweat"];
+        this.win = "";
     }
-
-
-
      winningWord(){
-        let win = this.goodwords[Math.floor(Math.Random()*10)]
+        this.win = this.goodwords[Math.floor(Math.Random()*10)]
     }
+     render() {
+    this.elem.style.top = "0px";
+    this.elem.style.left = this.xpos + "px";
+  }
 }
 
+// Some variables that we need below
+var boxes = [];
+var numBoxes = 250;
+var container = document.getElementById("container");
 
+// Dynamically create the boxes
+for(var i = 0; i < numBoxes; i++){
+    let animatedDiv = document.createElement("div");
+    animatedDiv.className = "box";
+    animatedDiv.id = "box" + i;
+    container.appendChild(animatedDiv);
+    boxes[i] = new Box(Math.random()*350,
+                       Math.random()*350,
+                       Math.random(),
+                       Math.random(),
+                       '#'+Math.floor(Math.random()*16777215).toString(16),
+                       animatedDiv.id);
+}
+
+var id = setInterval(frame, 5);
+
+// The animation code
+function frame() {
+    // Box 1
+    for (var i = 0; i < boxes.length; i++) {
+        if (boxes[i].xpos > 350 || boxes[i].xpos < 0) {
+            boxes[i].xstep = -boxes[i].xstep;
+        }
+        if (boxes[i].ypos > 350 || boxes[i].ypos < 0) {
+            boxes[i].ystep = -boxes[i].ystep;
+        }
+
+        boxes[i].xpos = boxes[i].xpos + boxes[i].xstep;
+        boxes[i].ypos = boxes[i].ypos + boxes[i].ystep;
+        boxes[i].render();
+    }
+}
 
 
 
