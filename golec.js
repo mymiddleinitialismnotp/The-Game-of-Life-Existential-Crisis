d5dfee9a43890ad75c3ac79a61ec7875ea3fbd5c
Kelly Robinett <kelly@Digitals-iMac-2.local>
2019-04-29 20:40:09 -0700

    Hi Mr. Temple, added remove box function and fixed css errors

--- golec.js
+++ golec.js
@@ -1,121 +1,51 @@
+class Game {
 
-class GameOfLife {
-    constructor(){
-        this.player = new Player()
-        window.addEventListener('keydown', (e) => {
-            if(e.keyCode == 37) {
-                console.log("move left!");
-                this.player.changeColumnPosition(-1);
-                this.player.render();
+}
 
-
-            }else if(e.keycode == 39) {
-                console.log(move)
-                this.player.changeColumnPosition(+1);
-                this.player.render();
-            }
-        }
-
-
-
-class Player {
-    constructor(_xpos, _ypos, _imgsrc) {
-        this.colPos = 2;
-        this.imgsrc = _imgsrc;
-        this.div = document.getElementById("player");
+class Box {
+    constructor(_xpos, _ypos, _xstep, _ystep, _id) {
+        this.xpos = _xpos;
+        this.ypos = _ypos;
+        this.xstep = _xstep;
+        this.ystep = _ystep;
+        this.elem = document.getElementById(_id);
+        this.height = "40px";
+        this.width = "100px";
     }
-
-    playerWordDeath() {
-
-    }
-
-    playerCatchWinnerWord() {
-
-    }
-
-    changeColumnPosition(delta){
-        this.colPos = this.colPos + delta;
-        console.log(this.colPos);
+    remove() {
+        let container = document.getElementById("container");
+        container.removeChild(this.elem);
     }
 
     render() {
-        let parent = this.div.parentNode;
-
-        if(this.colPos == 2) {
-             //This is where you need to remove it from one parent column
-            // And then append it as a child to a new parent column
-//https://www.w3schools.com/jsref/met_node_removechild.asp
-        }
-    }
-}
-
-
-
-
-class Anvil {
-    constructor(_xpos, _ypos, _imgsrc) {
-        this.xpos = _xpos;
-       this.ypos = _ypos;
-       this.imgsrc = _imgsrc;
+        this.elem.style.top = this.ypos + "px";
+        this.elem.style.left = this.xpos + "px";
     }
 
-     smashPlayer() {
-
-   }
 }
 
-
-
-
-class Word {
-    constructor(_xpos, _ypos, _ystep, _ystep, _id) {
+class Player {
+    constructor(_cont, _xpos, _ypos, _height, _width) {
+        this.container = _cont;
+        this.elem = document.getElementById("player");
         this.xpos = _xpos;
         this.ypos = _ypos;
-        this.ystep = _ystep;
-        this.elem = document.getElementById(_id);
-        this.goodWords = ["smallanimals", "batteries", "greenlights", "sleep", "icecream", "chickens", "smallplants", "payraise", "music", "art"];
-        this.badWords = ["anxiety", "depression", "taxes", "dictators", "eyebrowmites", "misspelleddwords", "moist", "tourists", "sneezing", "dirtydishes", "wetsocks", "weakhandshakes", "overexplaining", "studentdebt", "sweat"];
-        this.win = "";
+        this.height = _height;
+        this.width = _width;
     }
-     winningWord(){
-        this.win = this.goodwords[Math.floor(Math.Random()*10)]
+
+    move(event) {
+        this.xpos =
+            event.clientX -
+            this.container.getBoundingClientRect().left -
+            this.elem.clientWidth / 2;
+
+        this.render();
     }
-     render() {
-    this.elem.style.top = "0px";
-    this.elem.style.left = this.xpos + "px";
-  }
-}
 
-// Some variables that we need below
-var words = [];
-var numBoxes = Math.floor(Math.random()*5);
-var container = document.getElementById("body");
-
-// Dynamically create the boxes
-for(var i = 0; i < numBoxes; i++){
-    let animatedDiv = document.createElement("word");
-    animatedDiv.className = "box";
-    animatedDiv.id = "box" + i;
-    container.appendChild(animatedDiv);
-    boxes[i] = new Word(Math.random()*500,
-                      0,
-                       Math.random(),
-                       Math.random(),
-                       animatedDiv.id)
-}
-
-var id = setInterval(frame, 5);
-
-// The animation code
-function frame() {
-    // Box 1
-    for (var i = 0; i < boxes.length; i++) {
-        if (words[i].ypos > 350 || words[i].ypos < 0) {
-            words[i].ystep = -words[i].ystep;
-        }
-
-        words[i].ypos = words[i].ypos + words[i].ystep;
-        words[i].render();
+    render() {
+        this.elem.style.left = this.xpos + "px";
+        // render anything else you would like.
     }
 }
 
@@ -136,25 +66,125 @@ class Healthbar {
     }
 }
 
+// Some variables that we need below
+var boxes = [];
+var numBoxes = Math.ceil(Math.random() * 1);
+var container = document.getElementById("container");
+let goodWords = [
+  "smallanimals",
+  "batteries",
+  "greenlights",
+  "sleep",
+  "icecream",
+  "chickens",
+  "smallplants",
+  "payraise",
+  "music",
+  "art"
+];
+let badWords = [
+  "anxiety",
+  "depression",
+  "taxes",
+  "dictators",
+  "eyebrowmites",
+  "misspelleddwords",
+  "moist",
+  "tourists",
+  "sneezing",
+  "dirtydishes",
+  "wetsocks",
+  "weakhandshakes",
+  "overexplaining",
+  "studentdebt",
+  "sweat"
+];
+let allWords = [
+  "anxiety",
+  "depression",
+  "taxes",
+  "dictators",
+  "eyebrowmites",
+  "misspelleddwords",
+  "moist",
+  "tourists",
+  "sneezing",
+  "dirtydishes",
+  "wetsocks",
+  "weakhandshakes",
+  "overexplaining",
+  "studentdebt",
+  "sweat",
+  "smallanimals",
+  "batteries",
+  "greenlights",
+  "sleep",
+  "icecream",
+  "chickens",
+  "smallplants",
+  "payraise",
+  "music",
+  "art"
+];
 
-let myApp = new GameOfLife();
+// Dynamically create the boxes
+for (var i = 0; i < numBoxes; i++) {
+    let animatedDiv = document.createElement("div");
+    //create a text element
+    let textContent = document.createElement("P");
+    animatedDiv.className = "box";
+    animatedDiv.id = "box" + i;
+    //randomly choose a word from the array to reference the class to give to the text element
+    textContent.className = allWords[Math.floor(Math.random() * 25)];
+    container.appendChild(animatedDiv);
+    //append the text content to the animated div
+    animatedDiv.appendChild(textContent);
+    //creating a new object to push into boxes array with specific attributes
+    boxes[i] = new Box(
+        Math.random() * 350,
+        0,
+        0,
+        Math.random() + 5,
+        animatedDiv.id
+    );
+}
+
+
+//sets animation frame
+var id = setInterval(frame, 5);
+
+// The animation code
+function frame() {
+    // Box 1
+    for (var i = 0; i < boxes.length; i++) {
+        if (boxes[i].ypos > 365) {
+            boxes[i].remove();
+            boxes.splice(i, 1);
+
+        } else if (boxes[i].ypos < 0) {
+            boxes[i].ystep = -boxes[i].ystep;
+        }
+        //supposed to be the collision algorithm
+        for (var l = 0; l < boxes.length; l++) {
+            if (
+                boxes[i].xpos < player.xpos + player.width &&
+                boxes[i].xpos + boxes[i].width > player.xpos &&
+                boxes[i].ypos < player.ypos + player.height &&
+                boxes[i].ypos + boxes[i].height > player.ypos
+            ) {
+                // collision detected!
+                document.write("collision!");
+            }
+        }
+        boxes[i].ypos = boxes[i].ypos + boxes[i].ystep;
+        boxes[i].render();
+    }
+}
+
+
+let player = new Player(container, 255, 350, 50, 50);
+container.addEventListener("click", event => {
+    player.move(event);
+});
+//let myApp = new GameOfLife();
 let hb = new Healthbar();
-
-var rect1 = {x: 5, y: 5, width: 50, height: 50}
-var rect2 = {x: 20, y: 10, width: 10, height: 10}
-
-if (rect1.x < rect2.x + rect2.width &&
-   rect1.x + rect1.width > rect2.x &&
-   rect1.y < rect2.y + rect2.height &&
-   rect1.y + rect1.height > rect2.y) {
-    // collision detected!
-}
-
-// filling in the values =>
-
-if (5 < 30 &&
-    55 > 20 &&
-    5 < 20 &&
-    55 > 10) {
-    // collision detected!
-}
