5e22aac0cf28db3de391d224f51c1c8fad6cc513
mymiddleinitialismnotp <heykylamay@gmail.com>
2019-05-02 08:11:44 -0700

    js mod no big

--- golec.js
+++ golec.js
@@ -62,7 +62,7 @@ class Game {
                         that.boxes[i].removeBox();
                         that.boxes.splice(i, 1);
                         that.healthbar.updateHealth(5);
-                        alert("collision");
+                        window.alert("collision");
                     }
                 }
             },
